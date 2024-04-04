a3e7ada41b16c75823e2905ddf48f67f56ba9882
StrShell <157345423+StrShell@users.noreply.github.com>
2024-04-04 15:57:54 +0900

    Create openswan.sh

---
+++ aws/openswan.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+yum install -y openswan
+
+cat <<EOF >> /etc/sysctl.conf
+  net.ipv4.conf.all.accept_redirects = 0
+  net.ipv4.conf.all.send_redirects = 0
+EOF
+
+sysctl -p /etc/sysctl.conf
+
+cat <<EOF > /etc/ipsec.d/aws.conf
+conn Tunnel1
+	authby=secret
+	auto=start
+	left=%defaultroute
+	leftid=52.78.82.59
+	right=13.124.200.153
+	type=tunnel
+	ikelifetime=8h
+	keylife=1h
+	phase2alg=aes128-sha1;modp1024
+	ike=aes128-sha1;modp1024
+#	auth=esp (제거)
+	keyingtries=%forever
+	keyexchange=ike
+	leftsubnet=10.2.0.0/16
+	rightsubnet=10.1.0.0/16
+	dpddelay=10
+	dpdtimeout=30
+	dpdaction=restart_by_peer
+ EOF
+
+cat <<EOF > /etc/ipsec.d/aws.secrets
+$left $right: PSK "password"
+
+systemctl start ipsec
+systemctl enable ipsec
