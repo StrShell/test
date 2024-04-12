aa8782232482b6a96138b9cc78029acbbef8751e
StrShell <157345423+StrShell@users.noreply.github.com>
2024-04-12 13:23:30 +0900

    Update nfs-server.sh

--- aws/nfs-server.sh
+++ aws/nfs-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir /nfs-client
-mount -t nfs 10.1.1.100:/nfs-shared /nfs-client
-ec2-metadata -o | cut -d ' ' -f 2 >> /nfs-client/list.txt
-umount 10.1.1.100:/nfs-shared
-rmdir /nfs-client
+mkdir /nfs-shared
+chmod 777 /nfs-shared
+echo "/nfs-shared 10.0.0.0/14(rw,sync)" >> /etc/exports
+systemctl restart nfs-server
+systemctl enable nfs-server
+ec2-metadata -o | cut -d ' ' -f 2 >> /nfs-shared/list.txt
+chmod 777 /nfs-shared/list.txt
